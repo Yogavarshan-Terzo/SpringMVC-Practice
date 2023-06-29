bbb6c41e62aabf361894eaf82289dc5af4cc2f75
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-29 21:36:27 +0530

    Search Clubs

--- src/main/java/com/rungroop/web/repository/ClubRepository.java
+++ src/main/java/com/rungroop/web/repository/ClubRepository.java
@@ -1,10 +1,16 @@
 package com.rungroop.web.repository;
 
+import com.rungroop.web.dto.ClubDto;
 import com.rungroop.web.models.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClubRepository extends JpaRepository<Club,Long> {
     Optional<Club> findByTitle(String url);
+    @Query("SELECT C FROM Club C WHERE C.title LIKE CONCAT('%',:query,'%')")
+    List<Club> searchClubs(String query);
+
 }
--- src/main/java/com/rungroop/web/service/ClubService.java
+++ src/main/java/com/rungroop/web/service/ClubService.java
@@ -14,4 +14,5 @@ public interface ClubService {
     void updateClub(ClubDto clubDto);
 
     void delete(Long clubId);
+    List<ClubDto> searchClubs(String query);
 }
