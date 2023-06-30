550f88314581bb93ea27af3be0d48a9d58caa49f
Yogavarshan-Terzo <yogavarshan@terzocloud.com>
2023-06-30 10:27:18 +0530

    Create Event Form

--- src/main/java/com/rungroop/web/dto/EventDto.java
+++ src/main/java/com/rungroop/web/dto/EventDto.java
@@ -9,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 @Data
@@ -18,7 +19,9 @@ import java.time.LocalDateTime;
 public class EventDto {
     private Long id;
     private String name;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime startTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime endTime;
     private String type;
     private String photoUrl;
---
+++ src/main/java/com/rungroop/web/service/impl/EventServiceImpl.java
@@ -0,0 +1,42 @@
+package com.rungroop.web.service.impl;
+
+import com.rungroop.web.dto.EventDto;
+import com.rungroop.web.models.Club;
+import com.rungroop.web.models.Event;
+import com.rungroop.web.repository.ClubRepository;
+import com.rungroop.web.repository.EventRepository;
+import com.rungroop.web.service.EventService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventServiceImpl implements EventService {
+    ClubRepository clubRepository;
+    EventRepository eventRepository;
+
+    public EventServiceImpl(ClubRepository clubRepository, EventRepository eventRepository) {
+        this.clubRepository = clubRepository;
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public void createEvent(Long clubId, EventDto eventDto) {
+        Club club = clubRepository.findById(clubId).get();
+        Event event = mapToEvent(eventDto);
+        event.setClub(club);
+        eventRepository.save(event);
+    }
+
+    private Event mapToEvent(EventDto eventDto) {
+        Event event = Event.builder()
+                .id(eventDto.getId())
+                .name(eventDto.getName())
+                .startTime(eventDto.getStartTime())
+                .endTime(eventDto.getEndTime())
+                .type(eventDto.getType())
+                .photoUrl(eventDto.getPhotoUrl())
+                .createdOn(eventDto.getCreatedOn())
+                .updatedOn(eventDto.getUpdatedOn())
+                .build();
+        return event;
+    }
+}
